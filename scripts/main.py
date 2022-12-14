61982fdec7afec0a8a63b3bc02de078d7c3f436e
bnjasim <bnjasim@gmail.com>
2022-12-14 16:38:54 +0530

    all three levels added

--- scripts/main.py
+++ scripts/main.py
@@ -18,13 +18,13 @@ class Game:
 def print_congrats(hero_action):
     r = random.random()
     if r < 0.25:
-        print("Great Hit!!")
+        print("Congrats. That was a Great Hit!!")
     elif r < 0.5:
-        print("Nice Move!!")
+        print("Nice Move!! Hit Sebacc harder!")
     elif r < 0.75:
-        print("You " + hero_action + " well!")
+        print("You " + hero_action + " well! Finish him off")
     else:
-        print("Sebbac got Damage!")
+        print("Well done! Sebbac got Damage!")
 
 def print_sorry(sebbac_action):
     r = random.random()
@@ -33,20 +33,24 @@ def print_sorry(sebbac_action):
     elif r < 0.5:
         print("Bad.... Move!! Absorb the damage")
     elif r < 0.75:
-        print("Sebbac " + hero_action + " you!")
+        print("Sebbac " + sebbac_action + " you! Get back on your feet!")
     else:
         print("You got some Damage! Get up & fight!!")
 
+
+#============================================================================================
+# Level Two
+#============================================================================================
 
 def levelOne(game_state):
     print("""
        +-+-+-+-+-+ +-+-+-+          
        |L|E|V|E|L| |O|N|E|          
-    +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
-    |H|A|C|K|M|A|N| |V|S| |S|E|B|B|A|C|
-    +-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
-
++-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+|H|A|C|K|M|A|N| |V|S| |S|E|B|B|A|C|
++-+-+-+-+-+-+-+ +-+-+ +-+-+-+-+-+-+
     """)
+
     game_state.level = 1
     print("You have the powers of S-Shu (increased stamina) & H-Horus (increased speed)")
     print("Do you want to avail 'S-H' powers of SHAZAM? (you will have lesser time to fight though)")
@@ -63,10 +67,10 @@ def levelOne(game_state):
     sebbac_health = 100
 
     # weapons accuracy & power
-    hero_accuray = [0.7, 0.5, 0.3, 0.8]
+    hero_accuray = [0.7, 0.5, 0.2, 0.8]
     hero_power = [10, 20, 30]
     # the accuracy & power of sebbac & hero are the same in level 1
-    sebbac_accuray = [0.7, 0.5, 0.3, 0.8]
+    sebbac_accuray = [0.7, 0.5, 0.2, 0.8]
     sebbac_power = [10, 20, 30]
 
     print("\n Fasten your trousers! Fighting Begins!!!!! ")
@@ -97,9 +101,15 @@ def levelOne(game_state):
                     sebbac_got_hit = True
                 elif random.random() > sebbac_accuray[3]:
                     # if sebbacc failed to block
-                    print("Block failed!!")
+                    print("Block by Sebbac failed!!")
                     sebbac_got_hit = True
-        
+                else:
+                    print("Succesful block by Sebacc")
+            
+            else:
+                print("In-accurate action by You")
+
+
         # print a congratulatory message if sebbac got a hit
         if sebbac_got_hit:
             print_congrats(hero_action)
@@ -116,10 +126,10 @@ def levelOne(game_state):
         else:
             hero_action = "blocked"
         
+        print("You " + hero_action)
+
         # convert action to a number
         hero_action_index = actions.index(hero_action)
-
-        print("You " + hero_action)
 
         hero_got_hit = False
         if sebbac_action != "blocked":
@@ -130,8 +140,13 @@ def levelOne(game_state):
                     hero_got_hit = True
                 elif random.random() > hero_accuray[3]:
                     # if sebbacc failed to block
-                    print("Block failed!!")
+                    print("Your Block failed!!")
                     hero_got_hit = True
+                else:
+                    print("Your block was successful")
+
+            else:
+                print("In-accurate action by Sebbac")
         
         # print a sorry message if the hero got a hit
         if hero_got_hit:
@@ -169,7 +184,288 @@ def levelOne(game_state):
     
     else:
         print("Hawkman got killed! Dr. Fate will fight Sebbac in Level 2")
-        print("Good luck! See you Level Two")   
+        print("Good luck! See you Level Two") 
+        levelTwo(game_state) 
+
+
+#============================================================================================
+# Level Two
+#============================================================================================
+
+def levelTwo(game_state):
+    print("""
+            +-+-+-+-+-+ +-+-+-+             
+            |L|E|V|E|L| |T|W|O|             
+ +-+-+-+-+-+-+++-+-+-+-+++-+-+++-+-+-+-+-+-+
+ |D|O|C|T|O|R| |F|A|T|E| |V|S| |S|E|B|B|A|C|
+ +-+-+-+-+-+-+ +-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+    """)
+
+    game_state.level = 2
+    print("You have the powers of A-Amon (Increased power) & Z-Zehuti (Scholarly wisdom)")
+    print("You have S-Shu & H-Hours as well. (SHAZ of SHAZAM)")
+    hero_health = 120
+    allowed_time = 100
+    # Sebbac starts with a health of 100
+    sebbac_health = 100
+
+    # weapons accuracy & power
+    hero_accuray = [0.7, 0.5, 0.2, 0.8]
+    hero_power = [15, 25, 35]
+    # the power of sebbac is slightly lower than Dr. Fate in level 2
+    sebbac_accuray = [0.7, 0.5, 0.2, 0.8]
+    sebbac_power = [10, 20, 30]
+
+    print("\n Fasten your trousers Dr. Fate! Fighting Begins!!!!! ")
+
+    # start level one timer
+    start_time = time.time()
+    elapsed_time = 0
+
+    # list of weapons or actions
+    actions = ["punched", "kicked", "stabbed", "blocked"]
+    # Sebbac makes the first move!
+    hero_action = "blocked" # just a start
+    hero_action_index = 3
+    # flag to indicate who won the game
+    hero_won = False
+    while True:
+        # randomly sample an action for sebbac
+        sebbac_action = random.sample(actions, 1)[0]
+        print("Sebbac %s" % sebbac_action)
+        sebbac_action_index = actions.index(sebbac_action)
+        # check all possibilities of attacks on Sebbac
+        sebbac_got_hit = False
+        if hero_action != "blocked":
+            if random.random() < hero_accuray[hero_action_index]:
+                # the action of the hero is accurate
+                if sebbac_action != "blocked":
+                    # sebbac will take a hit
+                    sebbac_got_hit = True
+                elif random.random() > sebbac_accuray[3]:
+                    # if sebbacc failed to block
+                    print("Block by Sebbac failed!!")
+                    sebbac_got_hit = True
+                else:
+                    print("good block by Sebbac")
+            else:
+                print("In-accurate action by You")
+        
+        # print a congratulatory message if sebbac got a hit
+        if sebbac_got_hit:
+            print_congrats(hero_action)
+            sebbac_health -= hero_power[hero_action_index]
+            if sebbac_health <= 0:
+                # stop
+                hero_won = True
+                break
+
+        # decide hero action
+        action = int(input("Press 1 to punch, press 2 to kick, press 3 to stab, press 4 to block: "))
+        if action in [1, 2, 3]:
+            hero_action = actions[action-1]
+        else:
+            hero_action = "blocked"
+        
+        print("You " + hero_action)
+
+        # convert action to a number
+        hero_action_index = actions.index(hero_action)
+
+        hero_got_hit = False
+        if sebbac_action != "blocked":
+            if random.random() < sebbac_accuray[sebbac_action_index]:
+                # the action of sebbac is accurate
+                if hero_action != "blocked":
+                    # hero will take a hit
+                    hero_got_hit = True
+                elif random.random() > hero_accuray[3]:
+                    # if sebbacc failed to block
+                    print("Your Block failed!!")
+                    hero_got_hit = True
+                else:
+                    print("Your block was good!")
+            
+            else:
+                print("In-accurate action by Sebbac")
+
+        # print a sorry message if the hero got a hit
+        if hero_got_hit:
+            print_sorry(sebbac_action)
+            hero_health -= sebbac_power[sebbac_action_index]
+            if hero_health <= 0:
+                # stop
+                hero_won = False
+                break
+
+        print(f"Hero_health is {hero_health}")
+        print(f"Sebbac_health is {sebbac_health}")
+
+        # update elapsed time
+        elapsed_time = int(time.time() - start_time)
+        if elapsed_time < allowed_time:
+            print("Remaining Time: %d seconds" %(allowed_time - elapsed_time))
+        else:
+            print("Time over!")
+            hero_won = False
+            break
+    
+    # end of while loop
+    if hero_won:
+        print("Congratulations. Dr. Fate defeated Sebbac in Level 2! ")
+        print(f"Your score is {hero_health}")
+        print("You get a bonus of 100 points!")
+        hero_health += 100
+        if elapsed_time < (allowed_time - 20):
+            print("You finished Sebbac in %d seconds" %(elapsed_time))
+            print("You get a bonus of 50 points!")
+            hero_health += 50
+
+        print("Your total score is %d"%(hero_health))
+    
+    else:
+        print("Dr. Fate got killed! Black Adam will fight Sebbac in Level 3")
+        print("Good luck! See you Level Three")
+        levelThree(game_state)   
+
+
+
+#============================================================================================
+# Level Three
+#============================================================================================
+
+def levelThree(game_state):
+    print("""
+                  +-+-+-+-+-+ +-+-+-+-+-+         
+                  |L|E|V|E|L| |T|H|R|E|E|         
+         +-+-+-+-+-+++-+-+-+-+++-+-+++-+-+-+-+-+-+
+         |B|L|A|C|K| |A|D|A|M| |V|S| |S|E|B|B|A|C|
+         +-+-+-+-+-+ +-+-+-+-+ +-+-+ +-+-+-+-+-+-+
+        """)
+
+    game_state.level = 3
+    print("You have the powers of A-Aten (Impervious to magic spells) & M-Mehen (Supreme Accuracy)")
+    print("You have all the powers of SHAZAM in level 3")
+    hero_health = 120
+    allowed_time = 100
+    # Sebbac starts with a health of 100
+    sebbac_health = 100
+
+    # weapons accuracy & power
+    hero_accuray = [0.9, 0.9, 0.6, 0.8]
+    hero_power = [15, 25, 35]
+    # the power & accuracy of sebbac is lower than Black Adam in level 3
+    sebbac_accuray = [0.7, 0.5, 0.3, 0.8]
+    sebbac_power = [10, 20, 30]
+
+    print("\n Fasten your trousers Black Adam! Fighting Begins!!!!! ")
+
+    # start level one timer
+    start_time = time.time()
+    elapsed_time = 0
+
+    # list of weapons or actions
+    actions = ["punched", "kicked", "stabbed", "blocked"]
+    # Sebbac makes the first move!
+    hero_action = "blocked" # just a start
+    hero_action_index = 3
+    # flag to indicate who won the game
+    hero_won = False
+    while True:
+        # randomly sample an action for sebbac
+        sebbac_action = random.sample(actions, 1)[0]
+        print("Sebbac %s" % sebbac_action)
+        sebbac_action_index = actions.index(sebbac_action)
+        # check all possibilities of attacks on Sebbac
+        sebbac_got_hit = False
+        if hero_action != "blocked":
+            if random.random() < hero_accuray[hero_action_index]:
+                # the action of the hero is accurate
+                if sebbac_action != "blocked":
+                    # sebbac will take a hit
+                    sebbac_got_hit = True
+                elif random.random() > sebbac_accuray[3]:
+                    # if sebbacc failed to block
+                    print("Block by Sebbac failed!!")
+                    sebbac_got_hit = True
+                else:
+                    print("good block by Sebbac")
+            else:
+                print("In-accurate action by You")
+        
+        # print a congratulatory message if sebbac got a hit
+        if sebbac_got_hit:
+            print_congrats(hero_action)
+            sebbac_health -= hero_power[hero_action_index]
+            if sebbac_health <= 0:
+                # stop
+                hero_won = True
+                break
+
+        # decide hero action
+        action = int(input("Press 1 to punch, press 2 to kick, press 3 to stab, press 4 to block: "))
+        if action in [1, 2, 3]:
+            hero_action = actions[action-1]
+        else:
+            hero_action = "blocked"
+        
+        print("You " + hero_action)
+
+        # convert action to a number
+        hero_action_index = actions.index(hero_action)
+
+        hero_got_hit = False
+        if sebbac_action != "blocked":
+            if random.random() < sebbac_accuray[sebbac_action_index]:
+                # the action of sebbac is accurate
+                if hero_action != "blocked":
+                    # hero will take a hit
+                    hero_got_hit = True
+                elif random.random() > hero_accuray[3]:
+                    # if sebbacc failed to block
+                    print("Your Block failed!!")
+                    hero_got_hit = True
+                else:
+                    print("Your block was good!")
+            
+            else:
+                print("In-accurate action by Sebbac")
+
+        # print a sorry message if the hero got a hit
+        if hero_got_hit:
+            print_sorry(sebbac_action)
+            hero_health -= sebbac_power[sebbac_action_index]
+            if hero_health <= 0:
+                # stop
+                hero_won = False
+                break
+
+        print(f"Hero_health is {hero_health}")
+        print(f"Sebbac_health is {sebbac_health}")
+
+        # update elapsed time
+        elapsed_time = int(time.time() - start_time)
+        if elapsed_time < allowed_time:
+            print("Remaining Time: %d seconds" %(allowed_time - elapsed_time))
+        else:
+            print("Time over!")
+            hero_won = False
+            break
+    
+    # end of while loop
+    if hero_won:
+        print("Congratulations. Black Adam defeated Sebbac in Level 3! ")
+        print(f"Your score is {hero_health}")
+        if elapsed_time < (allowed_time - 20):
+            print("You finished Sebbac in %d seconds" %(elapsed_time))
+            print("You get a bonus of 50 points!")
+            hero_health += 50
+
+        print("Your total score is %d"%(hero_health))
+    
+    else:
+        print("Black Adam got killed! Sad Ending")
+         
 
 
 
